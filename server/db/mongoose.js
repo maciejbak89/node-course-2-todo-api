35b9ce9b66cecf4365ac8b271cf43ed24c7401c3
Maciej Bak <mbak704@gmail.com>
2019-02-13 02:13:41 -0600

    Setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,14 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
+
+// let db = {
+// 	localhost: 'mongodb://localhost:27017/TodoApp',
+// 	mlab: 'mongodb://maciejbak:DeJong21!@ds133865.mlab.com:33865/todoapp'
+// };
+
+// mongoose.connect(process.env.PORT ? db.mlab : db.localhost, {useNewUrlParser: true});
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {useNewUrlParser: true});
 
 module.exports = {mongoose};
