655c98c863d117e900234a94669c78b15bca9bc4
Maciej Bak <mbak704@gmail.com>
2019-02-14 02:41:48 -0600

    Added POST /users route and token method

---
+++ playground/hashing.js
@@ -0,0 +1,40 @@
+const {SHA256} = require('crypto-js');
+const jwt = require('jsonwebtoken');
+
+var data = {
+	id: 10
+};
+
+var token = jwt.sign(data, 'my-secret');
+console.log(token);
+
+var decoded = jwt.verify(token, 'my-secret');
+console.log('decoded: ', decoded);
+
+
+
+// var message = 'I am the administrator of this project.';
+// var hash = SHA256(message).toString();
+
+// console.log(`Message: ${message}`);
+// console.log(`Hash: ${hash}`);
+
+// var data = {
+// 	id: 4
+// };
+
+// var token = {
+// 	data,
+// 	hash: SHA256(JSON.stringify(data) + 'some_secret_SALT').toString()
+// };
+
+// token.data.id = 5;
+// token.hash = SHA256(JSON.stringify(token.data)).toString();
+
+// var resultHash = SHA256(JSON.stringify(token.data) + 'some_secret_SALT').toString();
+
+// if (resultHash === token.hash) {
+// 	console.log('Data was not changed.');
+// } else {
+// 	console.log('Data was changed. Do NOT trust!');
+// }
